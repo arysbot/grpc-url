0e2300036da0aed4ccf4ffcb8c3043c60515c255
torsina <theo.sagot.1@gmail.com>
2018-11-30 01:05:13 +0100

    fix path and bump version

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const fs = require("fs")
+const fs = require("fs");
+
 module.exports = (service) => {
     const NODE_ENV = process.env.NODE_ENV;
     if(process.env[`GRPC_HOST_${service}`]) {
         return process.env[`GRPC_HOST_${service}`];
     } else if(NODE_ENV === "TEST") {
-        const hostFile = fs.readFileSync("./hosts.json");
+        const hostFile = fs.readFileSync(`${__dirname}/hosts.json`);
         const hosts = JSON.parse(hostFile);
         return hosts.ip + hosts.ports[service];
     } else {
